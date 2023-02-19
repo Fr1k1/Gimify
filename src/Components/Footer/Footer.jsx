6f0accfe0c760e4d1b0244ed79a90bb2f75ded41
Fr1k1 <martin.friscic6@gmail.com>
2023-02-19 11:27:55 +0100

    Connected with fontawesome for footer icons

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faInstagram } from "@fortawesome/free-brands-svg-icons"
+
 import "./Footer.scss";
+
+library.add(faInstagram);
+
 
 const Footer = () => {
     return (
         <div id='footer_div'>
 
             <div id="red_line"></div>
+
+            <div id="icons">
+                <FontAwesomeIcon icon={{ prefix: 'fab', iconName: 'instagram' }} />
+            </div>
         </div>
     )
 }
