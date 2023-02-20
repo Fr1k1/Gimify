3dfab255476a53801dbcf5e7fcf14c7f4a70f35a
Fr1k1 <martin.friscic6@gmail.com>
2023-02-20 14:04:12 +0100

    Made code more reactive using Program component

--- src/Components/Program/Program.jsx
+++ src/Components/Program/Program.jsx
@@ -1,6 +1,22 @@
 import "./Program.scss";
+import { Link } from "react-router-dom";
 
-const Program = () => {
+
+const Program = ({ ikona, naslov, opis }) => {
+
+    return (<div className="program">
+        <span>{ikona}</span>
+        <span className='program_name' >{naslov}</span>
+        <span className='program_description' >{opis}</span>
+
+        <div id='button_div'>
+
+            <Link to={"/program/" + naslov}>
+
+                <button className='find_more_btn' >Saznajte više</button>
+            </Link>
+        </div>
+    </div>)
 
 }
 
--- src/Components/Programs/Programs.jsx
+++ src/Components/Programs/Programs.jsx
@@ -1,38 +1,18 @@
 import React from 'react'
 import "./Programs.scss";
 import { programsData } from "../../Data/Programs_data";
-import { Link } from "react-router-dom";
+import Program from '../Program/Program';
 
 
-
-//u ovom divu se nalaze programi...program je zasebna komponenta (mozda kasnije)
 const Programs = () => {
     return (
         <div id='programs_div' >
 
-
-
             <div id="categories">
-
                 {programsData.map((program, i) => (     /*ovak trebam napraviti i za detalje bas*/
-                    <div className="program" key={i}>
-                        <span>{program.ikona}</span>
-                        <span className='program_name' >{program.naslov}</span>
-                        <span className='program_description' >{program.opis}</span>
-
-                        <div id='button_div'>
-
-                            <Link to={"/program/" + program.naslov}>
-
-                                <button className='find_more_btn' >Saznajte više</button>
-                            </Link>
-                        </div>
-                    </div>
+                    <Program ikona={program.ikona} naslov={program.naslov} opis={program.opis} />
                 ))}
-
             </div>
-
-
 
         </div>
     )
