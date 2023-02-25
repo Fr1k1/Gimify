278d200d2369c0e1964c61abe599e0ab88fb048b
Fr1k1 <martin.friscic6@gmail.com>
2023-02-25 15:10:43 +0100

    Added responsive mobile navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.scss";
 import { Link } from "react-router-dom";
+import Bars from "../../assets/bars_navbar.png";
 
 function OtherPage() {
-
 
     setTimeout(() => {
         const element = document.getElementById('programs_div');
@@ -21,19 +21,41 @@ function OtherPage() {
 
 
 const Navbar = () => {
+
+    const mobile = window.innerWidth <= 768 ? true : false;
+    const [menuOpened, setMenuOpened] = useState(false);
+
     return (
-        <div className='navbar_parent'>
-            <ul id='navbar'>
-                <li><Link to="/">Početna</Link></li>
-
-                <li><Link to="http://localhost:3000/#programs_div" onClick={OtherPage}>Programi</Link></li>
-                <li><Link to="/gallery">Galerija</Link></li>
-                <li><Link to="/contact">Kontakt</Link></li>
-
-            </ul>
-
-        </div>
-    )
+        <>
+          {menuOpened === false && mobile === true ? (
+            
+            <div id='bars_div' onClick={()=>setMenuOpened(true)}>
+            <img src={Bars} alt="" id="bars" ></img>
+            </div>
+          ) : (
+            <div className="navbar_parent">
+              <ul id="navbar">
+                <li onClick={()=>setMenuOpened(false)}>
+                  <Link to="/">Početna</Link>
+                </li>
+      
+                <li onClick={()=>setMenuOpened(false)}>
+                  <Link to="http://localhost:3000/#programs_div" onClick={OtherPage}>
+                    Programi
+                  </Link>
+                </li>
+                <li onClick={()=>setMenuOpened(false)}>
+                  <Link to="/gallery">Galerija</Link>
+                </li>
+                <li onClick={()=>setMenuOpened(false)}>
+                  <Link to="/contact">Kontakt</Link>
+                </li>
+              </ul>
+            </div>
+          )}
+        </>
+      );
+      
 }
 
 export default Navbar
